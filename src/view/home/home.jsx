be84ec9699f6faefe452b6c2caf2e300dec0b146
LAPTOP-F2U3HPDF\18618 <18618404236@163.com>
2021-06-02 00:14:06 +0800

    v0.1.1

--- src/view/home/home.jsx
+++ src/view/home/home.jsx
@@ -1,15 +1,19 @@
 import './home.scss';
+import menu from '../../assets/menu.json';
 import React from 'react';
 import logo from './logo.svg';
 import { Menu } from 'antd';
-import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
-
 const { SubMenu } = Menu;
 
 export default class Home extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {current: 'mail'};
+        this.state = {
+            current: 'mail',
+            theme: 'dark',
+            mode: 'horizontal',
+            menu: menu
+        };
     }
 
     handleClick () {
@@ -20,28 +24,22 @@ export default class Home extends React.Component {
         return (
             <div className="home">
                 <menu className="menu">
-                    <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
-                        <Menu.Item key="mail" icon={<MailOutlined />}>
-                        Navigation One
-                        </Menu.Item>
-                        <Menu.Item key="app" disabled icon={<AppstoreOutlined />}>
-                        Navigation Two
-                        </Menu.Item>
-                        <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Navigation Three - Submenu">
-                        <Menu.ItemGroup title="Item 1">
-                            <Menu.Item key="setting:1">Option 1</Menu.Item>
-                            <Menu.Item key="setting:2">Option 2</Menu.Item>
-                        </Menu.ItemGroup>
-                        <Menu.ItemGroup title="Item 2">
-                            <Menu.Item key="setting:3">Option 3</Menu.Item>
-                            <Menu.Item key="setting:4">Option 4</Menu.Item>
-                        </Menu.ItemGroup>
-                        </SubMenu>
-                        <Menu.Item key="alipay">
-                        <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-                            Navigation Four - Link
-                        </a>
-                        </Menu.Item>
+                    <Menu 
+                        onClick={this.handleClick} 
+                        selectedKeys={[this.state.current]} 
+                        theme={this.state.theme}
+                        mode={this.state.mode}
+                        className="flex"
+                    >
+                        {this.state.menu.map((comment) => (
+                            comment.children
+                            ? <SubMenu key={comment.name} title={comment.content}>
+                                {comment.children.map((element) => (
+                                    <Menu.Item key={element.name}>{element.content}</Menu.Item>
+                                ))}
+                            </SubMenu>
+                            : <Menu.Item key={comment.name}>{comment.content}</Menu.Item>
+                        ))} 
                     </Menu>
                 </menu>
                 <header className="home-header">
