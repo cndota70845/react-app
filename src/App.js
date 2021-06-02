c0d19b0919078a3f73ba5bfa998e907093174ec6
LAPTOP-F2U3HPDF\18618 <18618404236@163.com>
2021-06-03 00:11:07 +0800

    redux v0.1.3

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import './App.css';
 import 'antd/dist/antd.css';
 import React from 'react';
 import Home from './view/home/home.jsx';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import todoApp from '../src/store/reducers/index.js';
+
+const store = createStore(todoApp);
 
 export default class App extends React.Component {
   render () {
     return (
-      <div className="App">
-        <Home></Home>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <Home></Home>
+        </div>
+      </Provider>
     )
   } 
 }
