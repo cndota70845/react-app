c7288aa57f218fc047f03ddf3926c30ee24ffced
ils <ly@bjtu.edu.cn>
2021-06-11 19:48:11 +0800

    login

--- src/view/login/login.jsx
+++ src/view/login/login.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
-import './login.scss';
+import style from './login.module.scss';
+// import { Form, Input, Button, Checkbox } from 'antd';
+
+class lisFrom {
+    constructor (opt) {
+        this.source = opt.source;
+    }
+}
+
+const test = new Promise(function (resolve, reject) {
+    setTimeout(function(){
+        resolve('ok'); //代码正常执行！
+    }, 2000);
+});
 
 export default class NotFound extends React.Component {
     constructor (props) {
         super(props);
         this.state = {
-            name: 'login'
+            name: 'login',
+            form: new lisFrom({source:{name:'第一项'}})
+        }
+    }
+
+    async componentDidMount () {
+        const res = await test;
+        if (res && res === 'ok') {
+            console.log('执行');
+            this.state.form.source.name = '第二项'
         }
     }
 
     render () {
         return (
-            <h1>this is {this.state.name} page</h1>
+            <div className={style.home}>
+                <div className={style.box}>
+                    <h1>{this.state.form.source.name}</h1>
+                </div>
+            </div>
         )
     }
 }
